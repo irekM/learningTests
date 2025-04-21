eefee1da3d56bd823d801cc3fd2e54b64c509748
IrekM <12935670+irekM@users.noreply.github.com>
2025-04-22 00:50:10 +0200

    add tests for currencyForm component

--- src/components/CurrencyForm/CurrencyForm.test.js
+++ src/components/CurrencyForm/CurrencyForm.test.js
@@ -13,5 +13,30 @@ describe('Component CurrencyForm', () => {
 
     // render component
     render(<CurrencyForm action={action} />);
+
+    //find "convert" button
+    const submitButton = screen.getByText('Convert');
+
+    //find field elems
+    const amountField = screen.getByTestId('amount');
+    const fromField = screen.getByTestId('from-select');
+    const toField = screen.getByTestId('to-select');
+
+    //set test values to fields
+
+    userEvent.type(amountField, '100');
+    userEvent.selectOptions(fromField, 'PLN');
+    userEvent.selectOptions(toField, 'USD');
+
+    //simulate user click on "convert" button
+
+    userEvent.click(submitButton);
+    
+    
+
+    //check if action callback was called once and with proper argument
+    expect(action).toHaveBeenCalledTimes(1);
+    expect(action).toHaveBeenCalledWith({amount: 100, from: 'PLN', to: 'USD'});
+    
 });
 });
